c3e08bbd8c7e1f0bf87564ed6c2576c048a42965
lizr2004 <oilizr2004@gmail.com>
2017-11-25 12:33:58 +0800

    delete binary and in/out file

---
+++ delelf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function substr
+{
+    STRING_A=$1
+    STRING_B=$2
+
+    if [[ ${STRING_A/${STRING_B}//} == $STRING_A ]]
+    then
+        ## is not substring.
+        return 0
+    else
+        ## is substring.
+        return 1
+    fi
+}
+find -name "*.in" -delete
+find -name "*.out" -delete
+find -name "*.ans" -delete
+find -name "*.exe" -delete
+for fil in $(find `ls`)
+do
+	str=$(file ${fil})
+	# echo $str
+    if [[ ${str/"ELF"//} != $str ]]
+    then
+        ## is not substring.
+        echo $fil
+        rm $fil
+    fi
+done
