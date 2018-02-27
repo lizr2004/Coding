f6bdae4eb0a25cac8f9a8de177c8437d2c3d0249
lizr2004 <oilizr2004@gmail.com>
2018-02-27 11:55:19 +0800

    20180227

--- delelf.sh
+++ delelf.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-function substr
-{
-    STRING_A=$1
-    STRING_B=$2
-
-    if [[ ${STRING_A/${STRING_B}//} == $STRING_A ]]
-    then
-        ## is not substring.
-        return 0
-    else
-        ## is substring.
-        return 1
-    fi
-}
-rm "contest/XTYF-lzr.zip"
+rm -f "contest/XTYF-lzr.zip"
 find -name "*.in" -delete
 find -name "*.out" -delete
 find -name "*.ans" -delete
